f694d500451b9934901b209d144ce2dfd5935489
Chris Zubak-Skees <chriszs@gmail.com>
2018-01-24 23:33:33 -0500

    Just import the 2016 cycle until we can resolve the duplicates problem in a way that picks the lastest

--- load.sh
+++ load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 psql -U politics -h db.fivetwentyseven.com -c "TRUNCATE fec_committees;"
-for cycle in 18 16 14; do
+for cycle in 16; do
     wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cm"$cycle".zip"
     unzip "cm"$cycle".zip"
     psql -U politics -h db.fivetwentyseven.com -c "COPY fec_committees FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < cm.txt politics
