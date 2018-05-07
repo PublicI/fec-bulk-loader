849205e471e9827fdb096c2e408f182ea2bd8c9c
Rosie Cima <rosie@MAC-10-020.local>
2018-05-07 16:12:09 -0400

    upsert

--- load.sh
+++ load.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 psql -U politics -h db.fivetwentyseven.com -c "TRUNCATE fec_committees;"
-for cycle in 16; do
+for cycle in 02 04 08 10 12 14 16; do
     wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cm"$cycle".zip"
     unzip "cm"$cycle".zip"
-    psql -U politics -h db.fivetwentyseven.com -c "COPY fec_committees FROM STDIN WITH CSV DELIMITER '|' QUOTE E'\b'" < cm.txt politics
+    psql -U politics -h db.fivetwentyseven.com -f load.sql
     rm cm.txt
 done
---
+++ load.sql
@@ -0,0 +1,11 @@
+create temporary table fec_committees_temp (id text, name text, treasurer text, street_1 text, street_2 text, city text, state text, zip text, designation text, type text, party text, frequency text, org_type text, connected_org_name text, candidate_id text);
+
+create table if not exists fec_committees (id text, name text, treasurer text, street_1 text, street_2 text, city text, state text, zip text, designation text, type text, party text, frequency text, org_type text, connected_org_name text, candidate_id text);
+
+\copy fec_committees_temp from 'cm.csv' with CSV DELIMITER '|' QUOTE E'\b';
+
+delete from fec_committees using fec_committees_temp where fec_committees.id = fec_committees_temp.id;
+
+insert into fec_committees (id, name, treasurer, street_1, street_2, city, state, zip, designation, type, party, frequency, org_type, connected_org_name, candidate_id) select id, name, treasurer, street_1, street_2, city, state, zip, designation, type, party, frequency, org_type, connected_org_name, candidate_id from temp;
+
+drop table fec_committees_temp;
