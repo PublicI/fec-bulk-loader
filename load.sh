fc1d3cb20cffc7e90e24d2694b469bc6b109a051
Chris Zubak-Skees <chriszs@gmail.com>
2018-05-08 18:18:06 -0400

    Add committees from 2018 cycle

--- load.sh
+++ load.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 psql -U politics -h db.fivetwentyseven.com politics -c "delete from fec_committees;"
-for cycle in 02 04 08 10 12 14 16; do
+for cycle in 02 04 08 10 12 14 16 18; do
     wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cm"$cycle".zip"
     unzip "cm"$cycle".zip"
     psql -U politics -h db.fivetwentyseven.com politics -f load.sql
