a314b68983349e417cf63bbdd4327b9b2edf32d0
Chris Zubak-Skees <chriszs@gmail.com>
2018-06-07 15:32:01 -0400

    Also load candidate files

--- load.sh
+++ load.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 psql -U politics -h db.fivetwentyseven.com politics -c "delete from fec_committees;"
+psql -U politics -h db.fivetwentyseven.com politics -c "delete from fec_candidates;"
 for cycle in 02 04 06 08 10 12 14 16 18; do
     wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cm"$cycle".zip"
     unzip "cm"$cycle".zip"
+    wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cn"$cycle".zip"
+    unzip "cn"$cycle".zip"
     psql -U politics -h db.fivetwentyseven.com politics -f load.sql
     rm cm.txt
+    rm cn.txt
 done
--- load.sql
+++ load.sql
@@ -9,3 +9,7 @@ delete from fec_committees using fec_committees_temp where fec_committees.id = f
 insert into fec_committees (id, name, treasurer, street_1, street_2, city, state, zip, designation, type, party, frequency, org_type, connected_org_name, candidate_id) select id, name, treasurer, street_1, street_2, city, state, zip, designation, type, party, frequency, org_type, connected_org_name, candidate_id from fec_committees_temp;
 
 drop table fec_committees_temp;
+
+create table if not exists fec_candidates (cand_id text, cand_name text, cand_pty_affiliation text, cand_election_yr text, cand_office_st text, cand_office text, cand_office_district text, cand_ici text, cand_status text, cand_pcc text, cand_st1 text, cand_st2 text, cand_city text, cand_st text, cand_zip text);
+
+\copy fec_candidates from 'cn.txt' with CSV DELIMITER '|' QUOTE E'\b';
