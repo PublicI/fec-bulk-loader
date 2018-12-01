aaf3edbc261ac10824b0bc4190d28463a1af5f57
Chris Zubak-Skees <chriszs@gmail.com>
2018-12-01 12:45:58 -0500

    Fix file sql file naming issue, use fec.gov path instead of S3

--- candidates/load.sh
+++ candidates/load.sh
@@ -6,8 +6,8 @@ CYCLES="${CYCLES:-18 16 14 12 10 08 06 04 02 00}" #  98 96 94 92 90
 
 psql -U politics -h db.fivetwentyseven.com politics -c "delete from fec_candidates;"
 for cycle in $CYCLES; do
-    wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cn"$cycle".zip"
+    wget -N "https://www.fec.gov/files/bulk-downloads/20"$cycle"/cn"$cycle".zip"
     unzip "cn"$cycle".zip"
-    psql -U politics -h db.fivetwentyseven.com politics -f candidates.sql
+    psql -U politics -h db.fivetwentyseven.com politics -f load.sql
     rm cn.txt
 done
--- committees/load.sh
+++ committees/load.sh
@@ -6,8 +6,8 @@ CYCLES="${CYCLES:-18 16 14 12 10 08 06 04 02 00}" #  98 96 94 92 90
 
 psql -c "delete from fec_committees;"
 for cycle in $CYCLES; do
-    wget -N "https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/20"$cycle"/cm"$cycle".zip"
+    wget -N "https://www.fec.gov/files/bulk-downloads/20"$cycle"/cm"$cycle".zip"
     unzip "cm"$cycle".zip"
-    psql -f committees.sql
+    psql -f load.sql
     rm cm.txt
 done
