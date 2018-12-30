2578a98d4a5960b0c66395d29b9c60c644413ffa
Chris Zubak-Skees <chriszs@gmail.com>
2018-12-30 11:07:19 -0500

    unzip itcont from indiv

--- contributions/load.sh
+++ contributions/load.sh
@@ -7,7 +7,7 @@ CYCLES="${CYCLES:-18 16 14 12 10 08 06 04 02 00}" #  98 96 94 92 90
 psql -f load.sql
 for cycle in $CYCLES; do
     wget -nv -N "https://www.fec.gov/files/bulk-downloads/20"$cycle"/indiv"$cycle".zip"
-    unzip "itcont"$cycle".zip"
+    unzip "indiv"$cycle".zip" "itcont"$cycle".txt"
     psql -c "\COPY fec_bulk_contributions FROM 'itcont"$cycle".txt' WITH CSV DELIMITER '|' QUOTE E'\b';"
     rm "itcont"$cycle".txt"
 done
