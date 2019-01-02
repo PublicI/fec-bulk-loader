a79bf637d8bb23ab42a3cce5fa60b15c28c6a43a
Chris Zubak-Skees <chriszs@gmail.com>
2019-01-02 11:05:05 -0500

    Remove 2002 and 2000 from expenditure cycles, because those files don't exist

--- expenditures/load.sh
+++ expenditures/load.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CYCLES="${CYCLES:-18 16 14 12 10 08 06 04 02 00}" #  98 96 94 92 90
+CYCLES="${CYCLES:-18 16 14 12 10 08 06 04}" # 02 00 98 96 94 92 90
 
 psql -f load.sql
 for cycle in $CYCLES; do
